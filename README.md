a2e15f141a5afb2284d6dd2d7922a74664e20b2a
willflemmer <willflemmer@playerdata.com>
2023-07-21 15:30:03 +0100

    test(runner): a failure case

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# KeywordCheck
+# KeywordChecker
 
 TODO: Delete this and the text below, and describe your gem
 
--- lib/keyword_checker/capybara_client.rb
+++ lib/keyword_checker/capybara_client.rb
@@ -3,18 +3,15 @@ require "capybara/cuprite"
 require "capybara"
 require "capybara/dsl"
 
-module KeywordCheck
+module KeywordChecker
   class CapybaraClient # rubocop:disable Style/Documentation
     include Capybara::DSL
-    def initialize(remote:)
+    def initialize
       Capybara.javascript_driver = :cuprite
       Capybara.register_driver(:cuprite) do |app|
         Capybara::Cuprite::Driver.new(app, window_size: [1200, 800])
       end
       Capybara.current_driver = :cuprite
-      # page = Browser.new.page
-      # page.visit("http://www.google.com")
-      # puts(page.html)
     end
   end
 end
